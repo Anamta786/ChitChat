c924cf26dc9a3cda8ff39e826dbf90d8bc719de5
Anamta Khan <khananamta22@gmail.com>
2020-05-14 23:11:39 +0530

    UI updates added

--- client/app/bundles/islioChat/components/HelloWorld.jsx
+++ client/app/bundles/islioChat/components/HelloWorld.jsx
@@ -1,29 +1,39 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import Header from '../Header/Header';
+import RegistrationForm from './RegistrationForm/RegistrationForm';
 
-const HelloWorld = ({ name, updateName }) => (
-  <div>
-    <h3>
-      Hello User, {name}!
-    </h3>
-    <hr />
-    <form >
-      <label htmlFor="name">
-        Say hello to:
-      </label>
-      <input
-        id="name"
-        type="text"
-        value={name}
-        onChange={(e) => updateName(e.target.value)}
-      />
-    </form>
-  </div>
-);
-
-HelloWorld.propTypes = {
-  name: PropTypes.string.isRequired,
-  updateName: PropTypes.func.isRequired,
-};
+// const HelloWorld = ({ name, updateName }) => (
+//   <div>
+//     <h3>
+//       Hello User, {name}!
+//     </h3>
+//     <hr />
+//     <form >
+//       <label htmlFor="name">
+//         Say hello to:
+//       </label>
+//       <input
+//         id="name"
+//         type="text"
+//         value={name}
+//         onChange={(e) => updateName(e.target.value)}
+//       />
+//     </form>
+//   </div>
+// );
+//
+// HelloWorld.propTypes = {
+//   name: PropTypes.string.isRequired,
+//   updateName: PropTypes.func.isRequired,
+// };
+function HelloWorld () {
+  return (
+    <div className="App">
+      <Header />
+      <RegistrationForm />
+    </div>
+  )
+}
 
 export default HelloWorld
